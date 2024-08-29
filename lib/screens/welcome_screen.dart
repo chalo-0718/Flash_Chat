446d4f803745fa885accc02378e6561df14a12e4
hemiye <hemiyecha@gmail.com>
2024-08-29 15:19:57 +0300

    fourth edition

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -23,21 +23,16 @@ class _WelcomeScreenState extends State<WelcomeScreen>
       // upperBound: 100.0,
     );
 
-    animation = CurvedAnimation(parent: controller, curve: Curves.decelerate);
-    animation = ColorTween(begin: Colors.red, end: Colors.blue).animate(controller);
+    // animation = CurvedAnimation(parent: controller, curve: Curves.decelerate);
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white).animate(controller);
 
-    controller.reverse(from: 1.0);
+    controller.forward();
 
-    animation.addStatusListener((status) {
-      if (status == AnimationStatus.completed) {
-        controller.reverse(from: 1.0);
-      } else if (status == AnimationStatus.dismissed) {
-        controller.forward();
-      }
-    });
+
 
     controller.addListener(() {
       setState(() {});
+      print(animation.value);
     });
   }
 
@@ -50,7 +45,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -63,7 +58,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                   tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: animation.value * 100,
+                    height: 60.0,
                   ),
                 ),
                 Text(
