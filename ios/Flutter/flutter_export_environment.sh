798c018f12c83c7e07573996b6a6cced6aba1c6c
hemiye <hemiyecha@gmail.com>
2024-09-03 19:36:14 +0300

    web connection

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\O N E\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\O N E\Desktop\Flutter_Project\Flash_Chat"
+export "FLUTTER_APPLICATION_PATH=C:\Users\O N E\Desktop\Flutter_Project\flash_chat_firebase"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
